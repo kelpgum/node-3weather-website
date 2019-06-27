43bd93086c72332122f55ea0e9ca59f81837262b
shnjcsc <shnjcsc@outlook.com>
2019-06-27 09:43:15 -0400

    Add text to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -22,7 +22,11 @@ const forecast = (latitude, longitude, callback) => {
           " It is currently " +
           body.currently.temperature +
           " degrees out." +
-          " There is a " +
+          " The high is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
           body.currently.precipProbability +
           "% change of precipitation."
       );
